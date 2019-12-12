3edd74f1821a3caeb07db917a28bb88695bea106
Gabriel Hendergate <ghendergate@ad.coxpowertrain.com>
2019-12-12 09:37:27 +0000

    Implemented chexking to detect if input matrix has changed since inverse was last cached. Commented code.

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## Object that stores a matrix and can cache its inverse
         m <- NULL
+        # Set the input matrix
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        # Get the input matrix
         get <- function() x
+        # Calculate the inverse matrix
         setinverse <- function(x) {m <<- solve(x)}
+        # Get the cached inverse matrix
         getinverse <- function() m
         list(set = set, get = get,
              getinverse = getinverse,
@@ -18,17 +23,26 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## Try to read a cached inverse matrix
+        ## and calculate if not cached or if
+        ## input matrix changes
         m <- x$getinverse()
+        # If inverse matrix exists 
         if (!is.null(m)) {
-                message("getting cached data")
-                return(m)
+                # If identity matrix has unity diag
+                # i.e. check if matrix has changed
+                # since inverse was calculated
+                if (all(diag(x$get() %*% m) == 1)){
+                        message("getting cached data")
+                        return(m)
+                }
         }
+        # Calculated inverse matrix if not already cached 
+        # or if input matrix changes
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        x$setinverse(data)
+        # Return inverse matrix
+        x$getinverse()
 }
